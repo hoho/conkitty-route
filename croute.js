93a564675e063ea6acab9099b094d4216ce8286d
Marat Abdullin <dakota@brokenpipe.ru>
2014-07-18 18:40:08 +0400

    Nearly working except for template calls.

--- croute.js
+++ croute.js
@@ -1,4 +1,4 @@
-$C.route = (function(decodeURIComponent, undefined) {
+$C.route = (function(decodeURIComponent, encodeURIComponent, undefined) {
     'use strict';
 
     var defaultTitle,
@@ -76,8 +76,6 @@ $C.route = (function(decodeURIComponent, undefined) {
                                 newRoutes[route._id] = route;
                             }
                         }
-
-                        console.log(12312312321, newRoutes);
                     } else {
                         // No matches.
                         newRootRoute = undefined;
@@ -85,8 +83,9 @@ $C.route = (function(decodeURIComponent, undefined) {
                     }
 
                     for (i in currentRoutes) {
-                        if (!(i in newRoutes) || reloadCurrent) {
-                            unprocessRoute(currentRoutes[i]);
+                        route = currentRoutes[i];
+                        if (!(i in newRoutes) || reloadCurrent || !route._s || route._dataError) {
+                            unprocessRoute(route);
                         }
                     }
 
@@ -171,6 +170,11 @@ $C.route = (function(decodeURIComponent, undefined) {
     }
 
 
+    function isString(val) {
+        return typeof val === 'string';
+    }
+
+
     function checkRunning(yes) {
         if (!!running !== !!yes) {
             throw new Error('Running: ' + !!running);
@@ -178,10 +182,10 @@ $C.route = (function(decodeURIComponent, undefined) {
     }
 
 
-    function pushParam(part, params, pathParams/**/, type) {
+    function pushParam(part, params, allowDuplicates, pathParams/**/, type) {
         type = part.charAt(1) === '?' ? 2 : 1;
         part = part.substring(type);
-        if (part in params) {
+        if ((part in params) && !allowDuplicates) {
             throw new Error('Duplicate param');
         }
         params[part] = type;
@@ -190,7 +194,7 @@ $C.route = (function(decodeURIComponent, undefined) {
         return pathParams ? '(?:/([^/]+))' + (type === 2 ? '?' : '') : part;
     }
 
-    function parseParameterizedURI(uri) {
+    function parseParameterizedURI(uri, isDataURI) {
         var pathname,
             search,
             hash,
@@ -210,12 +214,12 @@ $C.route = (function(decodeURIComponent, undefined) {
         });
 
         for (i = 0; i < pathname.length; i++) {
-            if ((name = pathname[i])) {
+            if ((name = decodeURIComponent(pathname[i]))) {
                 pathExpr.push(
                         name.charAt(0) === ':' ?
-                        pushParam(name, params, pathParams)
+                        pushParam(name, params, isDataURI, isDataURI ? undefined : pathParams)
                         :
-                        '/' + name.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+                        (isDataURI ? name : '/' + name.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'))
                 );
             }
         }
@@ -237,14 +241,14 @@ $C.route = (function(decodeURIComponent, undefined) {
                 if (error) { throw new Error(error); }
 
                 queryParams[name] = value && value.charAt(0) === ':' ?
-                    pushParam(value, params)
+                    pushParam(value, params, isDataURI)
                     :
                     {value: value};
             }
         }
 
         if (hash) {
-            hash = hash.charAt(0) === ':' ? pushParam(hash, params) : {value: hash};
+            hash = hash.charAt(0) === ':' ? pushParam(hash, params, isDataURI) : {value: hash};
         }
 
         return {
@@ -288,7 +292,7 @@ $C.route = (function(decodeURIComponent, undefined) {
                 ok = constraint.indexOf(value) >= 0;
                 break;
 
-            case typeof constraint === 'string':
+            case isString(constraint):
                 ok = constraint === value;
                 break;
 
@@ -406,9 +410,10 @@ $C.route = (function(decodeURIComponent, undefined) {
         }
 
         $H.on(route, {
-            go: function() {
+            go: function(same) {
                 self._a = self.parent ? self.parent._a : true;
                 self._p = currentParams;
+                self._s = same;
             },
             leave: function() {
                 self._a = self._achild = self._p = null;
@@ -434,19 +439,155 @@ $C.route = (function(decodeURIComponent, undefined) {
     }
 
 
-    function processAction(goal, args, defaultParent, route/**/, i) {
+    function AjaxGet(uri, params) {
+        // TODO: Test params and throw errors when necessary.
+        var ajaxPathname,
+            ajaxQueryparams,
+            i,
+            j,
+            pathObj,
+            part,
+            val,
+            self = this;
+
+        uri = parseParameterizedURI(uri, true);
+
+        ajaxPathname = [];
+        ajaxQueryparams = [];
+
+        pathObj = uri.pathname[0];
+
+        for (i = 0; i < pathObj.length; i++) {
+            part = pathObj[i];
+            if (part.param) {
+                part = params[part.param];
+                if (part !== undefined) { part = encodeURIComponent(part); }
+            } else {
+                part = encodeURIComponent(part);
+            }
+
+            if (part !== undefined) {
+                ajaxPathname.push(part);
+            }
+        }
+
+        if ((pathObj = uri.search)) {
+            for (i in pathObj) {
+                part = pathObj[i];
+                i = encodeURIComponent(i);
+                if (part.param) {
+                    val = params[part.param];
+                    if (isArray(val)) {
+                        for (j = 0; j < val.length; j++) {
+                            ajaxQueryparams.push(i + '=' + encodeURIComponent(val[j]));
+                        }
+                        continue;
+                    }
+                } else {
+                    val = part.value;
+                }
+
+                if (val !== undefined) {
+                    ajaxQueryparams.push(i + '=' + encodeURIComponent(val));
+                }
+            }
+        }
+
+        ajaxPathname = '/' + ajaxPathname.join('/');
+        if (ajaxQueryparams.length) {
+            ajaxPathname += '?' + ajaxQueryparams.join('&');
+        }
+
+        self.ok = [];
+        self.error = [];
+
+        self._r = val = new XMLHttpRequest();
+
+        if (val) {
+            val.open('GET', ajaxPathname, true);
+            val.onreadystatechange = function() {
+                if (val.readyState === 4 /* complete */) {
+                    self._done = self._error = true;
+                    self._r = undefined;
+                    if (val.status === 200) {
+                        try {
+                            self._data = JSON.parse(val.responseText);
+                            self._error = false;
+                        } catch(e) {}
+                    }
+                    self.done();
+                }
+            };
+            val.send();
+        } else {
+            self._done = self._error = true;
+            self.done();
+        }
+    }
+
+
+    proto = AjaxGet.prototype;
+
+
+    proto.then = function(ok, error) {
+        var self = this;
+        ok && self.ok.push(ok);
+        error && self.error.push(error);
+        self.done();
+    };
+
+
+    proto.reject = function() {
+        var self = this;
+
+        if (!self._done) {
+            if (self._r) {
+                self._r.abort();
+                self._r = undefined;
+            }
+            self._done = self._error = true;
+            self.done();
+        }
+    };
+
+
+    proto.done = function() {
+        var self = this,
+            todo,
+            data;
+
+        if (self._done) {
+            if (self._error) {
+                todo = self.error;
+            } else {
+                todo = self.ok;
+                data = self._data;
+            }
+
+            while (todo.length) {
+                todo.shift()(data);
+            }
+        }
+    };
+
+
+    function processAction(goal, datas, defaultParent, route/**/, i, params) {
         if (isArray(goal)) {
             for (i = 0; i < goal.length; i++) {
-                processAction(goal[i], args, defaultParent, route);
+                processAction(goal[i], datas, defaultParent, route);
             }
         } else if (goal) {
-            console.log(5545545, goal, args, defaultParent, route);
+            params = route._p;
+            console.log(5545545, goal, params, datas, defaultParent, route);
         }
     }
 
 
     function ProcessRoute(route) {
-        var self = this,
+        if (route._data instanceof ProcessRoute) { return; }
+
+        var skip = (route._data !== undefined) && !route._dataError,
+            self = this,
             datas = self.datas = [],
             dataSources = route.data,
             i,
@@ -454,24 +595,27 @@ $C.route = (function(decodeURIComponent, undefined) {
             waiting = 0,
             action = route.action || '',
             actionParent = action.parent,
-            error,
-            done = function(index, data, /**/i, children, r) {
+            done = function(index, data, /**/i, children, r, error) {
                 if (index !== undefined) {
                     datas[index] = data;
                     waiting--;
                 }
 
                 if (!waiting && !self.rejected) {
-                    route._data = datas;
+                    error = route._dataError;
                     children = route.children;
 
-                    if (error) {
-                        processAction(action.error, [], actionParent, route);
-                    } else {
-                        processAction(action.success, datas, actionParent, route);
-                    }
+                    if (!skip) {
+                        route._data = datas;
 
-                    processAction(action.after, error ? [true] : [], actionParent, route);
+                        if (error) {
+                            processAction(action.error, [], actionParent, route);
+                        } else {
+                            processAction(isString(action) || action.template ? action : action.success, datas, actionParent, route);
+                        }
+
+                        processAction(action.after, error ? [true] : [], actionParent, route);
+                    }
 
                     if (!error) {
                         for (i = 0; i < children.length; i++) {
@@ -484,30 +628,42 @@ $C.route = (function(decodeURIComponent, undefined) {
                 }
             };
 
-        route._data = self;
+        if (!skip) {
+            route._data = self;
 
-        processAction(action.before, [], actionParent, route);
+            processAction(action.before, [], actionParent, route);
 
-        if (dataSources !== undefined) {
-            if (!isArray(dataSources)) { dataSources = [dataSources]; }
+            if (dataSources !== undefined) {
+                if (!isArray(dataSources)) {
+                    dataSources = [dataSources];
+                }
 
-            for (i = 0; i < dataSources.length; i++) {
-                d = dataSources[i];
-                if (isFunction(d)) { d = d(); }
+                for (i = 0; i < dataSources.length; i++) {
+                    d = dataSources[i];
 
-                datas.push(d);
-
-                (function(index) {
-                    if (d && isFunction(d.then)) {
-                        waiting++;
-                        d.then(function(ok) {
-                            done(index, ok);
-                        }, function() {
-                            error = true;
-                            done(index);
-                        });
+                    if (isString(d)) {
+                        d = new AjaxGet(d, route._p);
                     }
-                })(i);
+
+                    if (isFunction(d)) {
+                        d = d();
+                    }
+
+                    datas.push(d);
+
+                    if (d && isFunction(d.then)) {
+                        (function (index) {
+                            waiting++;
+                            d.then(function (ok) {
+                                done(index, ok);
+                            }, function () {
+                                delete route._data;
+                                route._dataError = true;
+                                done(index);
+                            });
+                        })(i);
+                    }
+                }
             }
         }
 
@@ -530,16 +686,20 @@ $C.route = (function(decodeURIComponent, undefined) {
     };
 
 
-    function unprocessRoute(route) {
+    function unprocessRoute(route/**/, children, r) {
+        children = route.children;
+        for (r in children) { unprocessRoute(children[r]); }
+
         // Stop processing route and remove associated nodes.
         if (route._data && isFunction(route._data.reject)) {
             route._data.reject();
         }
         delete route._data;
+        delete route._dataError;
 
         // Remove nodes.
     }
 
 
     return API;
-})(decodeURIComponent);
+})(decodeURIComponent, encodeURIComponent);

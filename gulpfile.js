b82c989f90673ab15c2e15143f546f189962f2ab
Marat Abdullin <dakota@brokenpipe.ru>
2014-07-30 19:56:01 +0400

    0.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,17 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var conkitty = require('gulp-conkitty');
 var concat = require('gulp-concat');
-var qunit = require('gulp-qunit');
 
 
-gulp.task('jshint', function() {
+gulp.task('eslint', function() {
     return gulp.src(['gulpfile.js', 'croute.js'])
         .pipe(eslint({
             rules: {
                 'quotes': [2, 'single'],
                 'no-shadow-restricted-names': 0,
                 'no-underscore-dangle': 0,
-                'no-use-before-define': [2, 'nofunc']
+                'no-use-before-define': [2, 'nofunc'],
+                'no-new': 0
             },
             env: {
                 'node': true,
@@ -27,6 +27,7 @@ gulp.task('jshint', function() {
         .pipe(eslint.format());
 });
 
+
 gulp.task('uglify', function() {
     return gulp.src(['croute.js'])
         .pipe(uglify({preserveComments: 'some'}))
@@ -34,10 +35,6 @@ gulp.task('uglify', function() {
         .pipe(gulp.dest('.'));
 });
 
-gulp.task('qunit', ['uglify'], function() {
-    return gulp.src('./test/*.html')
-        .pipe(qunit());
-});
 
 gulp.task('templates', function() {
     return gulp.src('./test/*.ctpl')
@@ -49,31 +46,37 @@ gulp.task('templates', function() {
         .pipe(gulp.dest('tmp'));
 });
 
+
 gulp.task('serve', ['templates'], function() {
-    var http = require('http'),
-        fs = require('fs'),
-        routes = {
+    var http = require('http');
+    var fs = require('fs');
+
+    var routes = {
             '/qunit.css': 'node_modules/qunitjs/qunit/qunit.css',
             '/qunit.js': 'node_modules/qunitjs/qunit/qunit.js',
             '/histery.js': require.resolve('histery'),
             '/tpl.js': 'tmp/js.js',
             '/croute.js': 'croute.js',
-            '/croute.min.js': 'croute.min.js'
+            '/croute.min.js': 'croute.min.js',
+            '/test.js': 'test/test.js',
+            '/test': 'test/test.html'
         };
 
-    http.createServer(function serve(req, res) {
+    var _server = http.createServer(function serve(req, res) {
         if (req.url.substring(0, 5) === '/api/') {
             setTimeout(function() {
                 res.end(JSON.stringify({some: 'data', rnd: Math.random(), url: req.url}));
             }, 1000);
         } else {
             var file = routes[req.url];
-            fs.createReadStream(file || 'test/index.html').pipe(res);
+            fs.createReadStream(file || 'test/tmp.html').pipe(res);
         }
     }).listen(3000, 'localhost');
+
+    _server.on('close', function() { console.log('Stopped server'); });
 
     console.log('Listening on port 3000');
 });
 
 
-gulp.task('default', ['jshint', 'uglify', 'qunit']);
+gulp.task('default', ['eslint', 'uglify']);

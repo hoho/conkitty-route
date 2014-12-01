b5898620bd7195c55085b65db6353644edb01daa
Marat Abdullin <dakota@brokenpipe.ru>
2014-12-02 00:17:06 +0300

    Bump the version up.

--- croute.js
+++ croute.js
@@ -1,5 +1,5 @@
 /*!
- * conkitty-route v0.6.1, https://github.com/hoho/conkitty-route
+ * conkitty-route v0.6.2, https://github.com/hoho/conkitty-route
  * (c) 2014 Marat Abdullin, MIT license
  */
 
